31581623d76df8c35bf03f71db87681a1e6ebdc7
JeffC <chenyiqun0523@gmail.com>
2018-05-29 22:09:03 -0700

    add delete route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,24 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
